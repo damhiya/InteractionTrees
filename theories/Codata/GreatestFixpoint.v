c7e9d883c0ce021e9552959944e65f861bccefef
damhiya <damhiya@gmail.com>
2021-07-27 15:08:34 +0900

    extract bisimF

--- theories/Codata/GreatestFixpoint.v
+++ theories/Codata/GreatestFixpoint.v
@@ -25,19 +25,25 @@ Definition J {A : Type} {x : A} (P : forall y : A, x = y -> Type) (d : P x eq_re
   | eq_refl => d
   end.
 
-Definition next_eq {I} (C : Container I I) (i : I) (x y : M C i)
-           (command : projT1 (unfoldM x) = projT1 (unfoldM y))
-           (r : Response C i (projT1 (unfoldM x))) :
-  next C i (projT1 (unfoldM x)) r = next C i (projT1 (unfoldM y)) (rew command in r) :=
+Definition next_eq {I O} (C : Container I O) (X Y : I -> Type)
+           (o : O)
+           (x : [ C ] X o) (y : [ C ] Y o)
+           (command : projT1 x = projT1 y)
+           (r : Response C o (projT1 x)) :
+  next C o (projT1 x) r = next C o (projT1 y) (rew command in r) :=
   J
-    (fun c (command : projT1 (unfoldM x) = c) => next C i (projT1 (unfoldM x)) r = next C i c (rew command in r))
+    (fun c (command : projT1 x = c) => next C o (projT1 x) r = next C o c (rew command in r))
     eq_refl
-    (projT1 (unfoldM y))
+    (projT1 y)
     command.
 
-CoInductive bisim {I} (C : Container I I) (i : I) (x y : M C i) := fold_bisim
-  { command : projT1 (unfoldM x) = projT1 (unfoldM y)
-  ; response : forall r, bisim C _
-                          (rew (next_eq C i x y command r) in projT2 (unfoldM x) r)
-                          (projT2 (unfoldM y) (rew command in r))
-  }.
+Variant bisimF {I O} (C : Container I O) (X Y : I -> Type) (R : forall i, X i -> Y i -> Type)
+        (o : O) (x : [ C ] X o) (y : [ C ] Y o) :=
+| BisimStep (command_eq : projT1 x = projT1 y)
+            (response_R : forall r, R (next C o (projT1 y) (rew command_eq in r))
+                                  (rew (next_eq C X Y o x y command_eq r) in (projT2 x r))
+                                  (projT2 y (rew command_eq in r)))
+.
+
+CoInductive bisim {I} (C : Container I I) (i : I) (x y : M C i) : Type := fold_bisim
+  { unfold_bisim : bisimF C (M C) (M C) (bisim C) i (unfoldM x) (unfoldM y) }.
