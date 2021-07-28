c3f7f4150bbb0d2f1ec4a2465759b7b241db8985
KiJeong-Lim <gijungdduk@naver.com>
2021-07-28 11:49:09 +0900

    shown bisimF is monotonic

--- theories/Codata/GreatestFixpoint.v
+++ theories/Codata/GreatestFixpoint.v
@@ -30,7 +30,42 @@ Variant bisimF_ (C : Container) (X : Type) (R : X -> X -> Type) : [ C ] X -> [ C
   : bisimF_ C X R (existT _ shape extension_l) (existT _ shape extension_r)
 .
 
+Lemma bisimF_intro {C : Container} {X : Type} {R : X -> X -> Type} :
+  forall shape : Shape C,
+  forall extension_l extension_r : Position C shape -> X,
+  (forall p, R (extension_l p) (extension_r p)) ->
+  bisimF_ C X R (existT _ shape extension_l) (existT _ shape extension_r).
+Proof.
+  exact (BisimStep C X R).
+Qed.
+
+Lemma bisimF_elim {C : Container} {X : Type} {R : X -> X -> Type} {lhs rhs} :
+  bisimF_ C X R lhs rhs ->
+  exists (shape : Shape C)
+         (extension_l extension_r : Position C shape -> X)
+         (REL : forall p, R (extension_l p) (extension_r p)),
+         lhs = existT _ shape extension_l /\ rhs = existT _ shape extension_r.
+Proof.
+  intros [shape extension_l extension_r REL].
+  exists shape, extension_l, extension_r, REL.
+  split; reflexivity.
+Qed.
+
 Definition bisimF (C : Container) (R : M C -> M C -> Prop) (x y : M C) :=
   bisimF_ C (M C) R (unfoldM x) (unfoldM y).
+
+Lemma monotone2_bisimF (C : Container) :
+  monotone2 (bisimF C).
+Proof.
+  apply monotone2_eq.
+  intros R1 R2 LE x y Hb.
+  destruct (bisimF_elim Hb) as [shape [extension_l [extension_r [REL [H1 H2]]]]].
+  unfold bisimF.
+  rewrite H1, H2.
+  apply (bisimF_intro shape).
+  intros p.
+  apply LE.
+  exact (REL p).
+Qed.
 
 Definition bisim (C : Container) := paco2 (bisimF C).
--- theories/Codata/IndexedGreatestFixpoint.v
+++ theories/Codata/IndexedGreatestFixpoint.v
@@ -31,7 +31,44 @@ Variant bisimF_ {I O} (C : Container I O) (X : I -> Type) (R : forall i, X i -> 
   : bisimF_ C X R o (existT _ command extension_l) (existT _ command extension_r)
 .
 
+Lemma bisimF_intro {I O} {C : Container I O} {X : I -> Type}
+      {R : forall i, X i -> X i -> Type} {o : O} :
+      forall command : Command C o,
+      forall extension_l extension_r : forall r, X (next C command r),
+      (forall r, R (next C command r) (extension_l r) (extension_r r)) ->
+      bisimF_ C X R o (existT _ command extension_l) (existT _ command extension_r).
+Proof.
+  exact (BisimStep C X R o).
+Qed.
+
+Lemma bisimF_elim {I O} {C : Container I O} {X : I -> Type}
+      {R : forall i, X i -> X i -> Type} {o : O} {lhs rhs} :
+      bisimF_ C X R o lhs rhs ->
+      exists (command : Command C o)
+             (extension_l extension_r : forall r, X (next C command r))
+             (REL : forall r, R (next C command r) (extension_l r) (extension_r r)),
+             lhs = existT _ command extension_l /\ rhs = existT _ command extension_r.
+Proof.
+  intros [command extension_l extension_r REL].
+  exists command, extension_l, extension_r, REL.
+  split; reflexivity.
+Qed.
+
 Definition bisimF {I} (C : Container I I) (R : forall i, M C i -> M C i -> Prop) (i : I) (x y : M C i) :=
   bisimF_ C (M C) R i (unfoldM x) (unfoldM y).
+
+Lemma monotone3_bisimF {I} (C : Container I I) :
+  monotone3 (bisimF C).
+Proof.
+  apply monotone3_eq.
+  intros R1 R2 LE i x y Hb.
+  destruct (bisimF_elim Hb) as [command [extension_l [extension_r [REL [H1 H2]]]]].
+  unfold bisimF.
+  rewrite H1, H2.
+  apply (bisimF_intro command).
+  intros r.
+  apply LE.
+  exact (REL r).
+Qed.
 
 Definition bisim {I} (C : Container I I) := paco3 (bisimF C).
